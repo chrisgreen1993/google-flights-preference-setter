5142ac9898b891a93cc7cb8575228c5a0deda3df
Chris Green <chrisgreen1993@gmail.com>
2025-05-22 00:47:49 +0700

    Make sure we add the preferences when we load a google flights tab

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,9 @@
-chrome.storage.onChanged.addListener(async (changes, namespace) => {
-  if (namespace !== "sync" || !changes.preferences) return;
-
-  const { preferences = {} } = changes;
-  const {
-    currency = "USD",
-    language = "en-US",
-    location = "US",
-  } = preferences.newValue;
+async function updateTabUrlWithPreferences(
+  tabId: number,
+  tabUrl: string,
+  preferences: { currency?: string; language?: string; location?: string },
+) {
+  const { currency = "USD", language = "en-US", location = "US" } = preferences;
 
   const queryParamMappings = {
     curr: currency,
@@ -14,24 +11,41 @@ chrome.storage.onChanged.addListener(async (changes, namespace) => {
     gl: location,
   };
 
+  const url = new URL(tabUrl);
+
+  const hasChanges = Object.entries(queryParamMappings).some(
+    ([param, value]) => url.searchParams.get(param) !== value,
+  );
+
+  if (hasChanges) {
+    Object.entries(queryParamMappings).forEach(([param, value]) => {
+      url.searchParams.set(param, value);
+    });
+    await chrome.tabs.update(tabId, { url: url.toString() });
+  }
+}
+
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync" || !changes.preferences) return;
+
+  const preferences = changes.preferences.newValue;
+
   const tabs = await chrome.tabs.query({
     url: "https://www.google.com/travel/flights*",
   });
 
   tabs.forEach(async (tab) => {
     if (!tab.url || !tab.id) return;
-    const url = new URL(tab.url);
-
-    const hasChanges = Object.entries(queryParamMappings).some(
-      ([param, value]) => url.searchParams.get(param) !== value,
-    );
-
-    if (hasChanges) {
-      Object.entries(queryParamMappings).forEach(([param, value]) => {
-        url.searchParams.set(param, value);
-      });
-
-      await chrome.tabs.update(tab.id, { url: url.toString() });
-    }
+    await updateTabUrlWithPreferences(tab.id, tab.url, preferences);
   });
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (
+    changeInfo.status === "loading" &&
+    tab.url?.startsWith("https://www.google.com/travel/flights")
+  ) {
+    const { preferences = {} } = await chrome.storage.sync.get("preferences");
+    await updateTabUrlWithPreferences(tabId, tab.url, preferences);
+  }
 });
